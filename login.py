0620b192c5774cd96f04e67fe0d5500b020907fb
Felipera-Dev <felipedaveigagomes18@gmail.com>
2025-05-31 15:18:49 -0300

    Secao 2 finalizada(Quebrando o sha256)

--- login.py
+++ login.py
@@ -1,6 +1,8 @@
  #Feito por Felipe da Veiga Gomes e Rogerio de Abreu Mar
 import json
 import hashlib
+import itertools
+import time
 
 def LerDadosUser():
     with open('usuarios.json') as file:
@@ -31,9 +33,13 @@ def cadastrar():
 
     print("Cadastro Concluido")
 
-def login():
+def login(user_login,senha_login):
     user_login = input("Digite seu nome:")
     senha_login = input("Digite sua senha:")
+
+    senha_login = senha_login.encode()
+    senha_login_hash = hashlib.sha256(senha_login)
+    senha_login_hash_hex = senha_login_hash.hexdigest()
 
     usuarios = LerDadosUser()
 
@@ -44,7 +50,7 @@ def login():
         senha_registrada = user["senha"]
 
         if user_registrado == user_login:
-            if senha_registrada == senha_login:
+            if senha_registrada == senha_login_hash_hex:
                 achou_user = True
                 achou_senha = True
 
@@ -61,7 +67,9 @@ def Secao1():
         if opcao == "1":
             cadastrar()
         elif opcao == "2":
-            login()
+            user_login = input("Digite seu nome:")
+            senha_login = input("Digite sua senha:")
+            login(user_login, senha_login)
         elif opcao == "3":
             print("Saindo do sistema...")
         else:
@@ -69,11 +77,29 @@ def Secao1():
 
 def Secao2():
     print("sessao 2")
-    usuarios  = LerDadosUser()
-    for user in usuarios:
-        print(user["senha"])
-
-
+    inicio_timer_total = time.time()
+    dados_usuarios  = LerDadosUser()
+    
+    for user in dados_usuarios:
+        print(f"Procurando senha do usuário: {user['user']}")
+        inicio_timer_user = time.time()
+        caracteres = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u', 'v', 'w', 'x', 'y', 'z',
+                  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z']
+        combinacoes = itertools.product(caracteres, repeat=4) # gerando combinações de 4 caracteres com plano cartesiano
+        for combinacao_tupla in combinacoes:
+                combinacao = ("".join(combinacao_tupla))#como as combinações são tuplas(plano cartesiano), foi utilizado o join para transformar em string
+                combinacao_hash = hashlib.sha256(combinacao.encode())
+                combinacao_hash_hex = combinacao_hash.hexdigest()
+                if user["senha"] == combinacao_hash_hex:
+                    fim_timer_user = time.time()
+                    print(f" - Usuario:{user['user']} Senha encontrada: {combinacao}")
+                    print(f" - Tempo de execução: {fim_timer_user - inicio_timer_user:.4f} segundos")
+                    break
+        
+    fim_timer_total = time.time()
+    print(f"Tempo total de execução: {fim_timer_total - inicio_timer_total:.4f} segundos")
+            
 
 opcao = 0
 while opcao != "0":
